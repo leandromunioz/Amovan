56d78e550b9be7403e336d0dc56db24a9450c1fa
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2023-09-20 07:17:31 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,4 @@
 
-        //Efecto logo móvil Amovan
-
-        document.addEventListener("DOMContentLoaded", function () {
-            const logoNombre = document.querySelector(".logo__nombre");
-
-            // Comprueba si la animación ya se ha reproducido durante esta sesión
-            const animationPlayed = sessionStorage.getItem("animationPlayed");
-
-            if (!animationPlayed) {
-                // Calcula el 10% de la altura de la ventana
-                const stopPosition = window.innerHeight * 0.10;
-
-                // Agrega un evento de animación que se activa al final de la animación
-                logoNombre.addEventListener("animationend", function () {
-                    // Establece el estado final de la animación
-                    logoNombre.style.animation = "none";
-
-                    // Marca que la animación ya se ha reproducido durante esta sesión
-                    sessionStorage.setItem("animationPlayed", "true");
-                });
-
-                // Comienza la animación
-                logoNombre.style.animationPlayState = "running";
-            }
-        });
-
-        //Fin efecto móvil Amovan
-
         
         //Comienzo efecto hidden
 
